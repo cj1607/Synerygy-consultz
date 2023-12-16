1d2e7abe9d1d455dc5856807d0fd2ce158f5c37a
ayoluwanimi <66927901+ayoluwanimi@users.noreply.github.com>
2023-12-16 10:21:26 +0100

    update

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.slider-nav').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        focusOnSelect: false,
+        autoplay: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        speed: 1000
+    });
+});
